72429019565eb6d018caaecda103833ef1d6f489
MackDoesDev <sebastian.ulbricht1980@gmail.com>
2020-02-23 12:58:47 +0800

    fix: final fix for Alex's script

--- AlexEDM/pdf-search-and-download/index.js
+++ AlexEDM/pdf-search-and-download/index.js
@@ -4,15 +4,30 @@ const http = require("http");
 const axios = require("axios");
 
 // URL for search endpoint
-const searchUrl = "http://words2.wordss.com:8080/SEARCH.ASP";
+const searchUrl = "http://example.com:8080/SEARCH.ASP";
 // HTTP method of the search endpoint
 const searchMethod = "post";
 // query parameter name for search endpoint (e.g., 'q' for https://www.google.com/search/?q=searchterm)
 const searchParamName = "SearchString";
-// list of search terms we want to send to the search endpoint
-const searchTerms = ["12554898", "23654895", "32564985"];
 
-sendSearchRequest(searchTerms).catch(console.error);
+const inputFileName = "list.txt";
+
+const searchTerms = fs
+  .readFileSync(path.join(__dirname, inputFileName))
+  .toString()
+  .trim()
+  .split("\n")
+  .map(term => term.trim());
+
+sendSearchRequest(searchTerms)
+  .then(() => {
+    console.log(`Total Searchterms: ${searchTerms.length}`);
+    console.log("No files found for the following number:");
+    filesNotFound.forEach(term => console.log(term));
+  })
+  .catch(console.error);
+
+const filesNotFound = [];
 
 async function sendSearchRequest(searchTerms) {
   for (searchTerm of searchTerms) {
@@ -21,18 +36,19 @@ async function sendSearchRequest(searchTerms) {
 }
 
 function handleSearchRequest(searchTerm) {
+  if (!searchTerm) {
+    return;
+  }
+
   return new Promise(async (resolve, reject) => {
     try {
       console.log(`Searching: ${searchTerm}`);
       const result = await axios.request(searchUrl, {
         method: searchMethod,
-        data: {
-          [searchParamName]: searchTerm,
-          Action: 1
-        }
+        data: `${searchParamName}=${searchTerm}&Action=Go`
       });
 
-      parseSearchResults(result.data);
+      parseSearchResults(result.data, searchTerm);
       return resolve();
     } catch (err) {
       return reject(err);
@@ -40,20 +56,43 @@ function handleSearchRequest(searchTerm) {
   });
 }
 
-const codeRegex = /<a[^>]*?javascript:NAF\('(.*?invoice\.pdf)'.*?\)[^>]*?>/gi;
-function parseSearchResults(result) {
-  console.log("RESULT:", result);
-  let match = codeRegex.exec(result);
+function parseSearchResults(result, searchTerm) {
+  const codeRegex = new RegExp(
+    `<a[^>]*?javascript:NAF\\('(.*?${searchTerm}.*?(?:invoice|credit memo)\\.pdf)'.*?\\)[^>]*?>`,
+    "gi"
+  );
+  let lastLink = "";
+  let found = false;
 
-  while (match !== null) {
-    downloadFile(match[1]);
-    codeRegex.exec(result);
+  while ((match = codeRegex.exec(result))) {
+    const link = match[1];
+    if (link != lastLink) {
+      downloadFile(match[1]);
+      found = true;
+      lastLink = link;
+    }
+  }
+
+  if (!found) {
+    filesNotFound.push(searchTerm);
   }
 }
 
-function downloadFile(url) {
+async function downloadFile(url) {
   console.log(`Downloading file: ${path.basename(url)}`);
   const filename = path.basename(url).replace(":", "_");
   const file = fs.createWriteStream(path.join(__dirname, "download", filename));
-  http.get(url, response => response.pipe(file));
+
+  const response = await axios.request({
+    url,
+    method: "GET",
+    responseType: "stream"
+  });
+
+  response.data.pipe(file);
+
+  return new Promise((resolve, reject) => {
+    file.on("finish", resolve);
+    file.on("error", reject);
+  });
 }
